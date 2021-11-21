713a03b2ffbf3fc78558ead7b455873b5ce40c2c
bilguun.e <bigeservice@gmail.com>
2021-11-21 17:21:02 +0800

    Updates

---
+++ precache-manifest.4be2b04ac3a42124b12c9f181d9f2a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dcb9820b3cdcec1740c56cf596b85a",
+    "url": "/shoppin-card/index.html"
+  },
+  {
+    "revision": "9509926d708344fbca26",
+    "url": "/shoppin-card/static/css/main.b809e878.chunk.css"
+  },
+  {
+    "revision": "766f179888298cf8dbf7",
+    "url": "/shoppin-card/static/js/2.ec9e2155.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppin-card/static/js/2.ec9e2155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9509926d708344fbca26",
+    "url": "/shoppin-card/static/js/main.8498213f.chunk.js"
+  },
+  {
+    "revision": "b25bdfa9b78f9105ee21",
+    "url": "/shoppin-card/static/js/runtime-main.69122c33.js"
+  },
+  {
+    "revision": "b5cf6a080a7f62988ea0c45b9d3087f3",
+    "url": "/shoppin-card/static/media/spiderman.b5cf6a08.jpg"
+  }
+]);
